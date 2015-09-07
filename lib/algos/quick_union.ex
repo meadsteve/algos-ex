05cc5d618f1c10eeea9f7a69cca434c53d736e05
steve brazier <meadsteve@gmail.com>
2015-09-07 16:59:33 +0000

    quick union implementation

--- lib/algos/quick_union.ex
+++ lib/algos/quick_union.ex
@@ -10,11 +10,25 @@ defmodule Algos.QuickUnion do
   end
 
   def union(%QuickUnion{data: data}, x, y) when is_integer(x) and is_integer(y) do
-  
+    root_x = get_root(data, x)
+    root_y = get_root(data, y)
+    updated_data = Map.put(data, root_x, root_y)
+    %QuickUnion{data: updated_data}
   end
 
   def connected?(%QuickUnion{data: data}, x, y) when is_integer(x) and is_integer(y) do
+    root_x = get_root(data, x)
+    root_y = get_root(data, y)
+    root_x == root_y
+  end
 
+  defp get_root(data, i) do
+    parent = data[i]
+    if parent == i do
+      i
+    else
+      get_root(data, parent)
+    end
   end
 
 end
