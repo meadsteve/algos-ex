a5652e880c2cb4e6bc18c9622507321457f18eff
steve brazier <meadsteve@gmail.com>
2015-09-07 17:23:41 +0000

    add weighting

--- lib/algos/quick_union.ex
+++ lib/algos/quick_union.ex
@@ -4,15 +4,19 @@ defmodule Algos.QuickUnion do
 
   def new(n) when is_integer(n) do
     initial = 0..n
-      |> Enum.reduce(Map.new, fn(i, map) -> map |> Map.put(i, i) end)
+      |> Enum.reduce(Map.new, fn(i, map) -> map |> Map.put(i, {i, 1}) end)
 
     %QuickUnion{data: initial}
   end
 
   def union(%QuickUnion{data: data}, x, y) when is_integer(x) and is_integer(y) do
-    root_x = get_root(data, x)
-    root_y = get_root(data, y)
-    updated_data = Map.put(data, root_x, root_y)
+    {root_x, size_x} = get_root(data, x)
+    {root_y, size_y} = get_root(data, y)
+    updated_data = if (size_y > size_x) do
+      data |> update_root(root_y, root_x, (size_x + size_y))
+    else
+      data |> update_root(root_x, root_y, (size_x + size_y))
+    end
     %QuickUnion{data: updated_data}
   end
 
@@ -23,12 +27,18 @@ defmodule Algos.QuickUnion do
   end
 
   defp get_root(data, i) do
-    parent = data[i]
+    {parent, size} = data[i]
     if parent == i do
-      i
+      {parent, size}
     else
       get_root(data, parent)
     end
+  end
+
+  defp update_root(data, root, old_root, size) do
+    data
+      |> Map.put(old_root, {root, 0})
+      |> Map.put(root, {root, size})
   end
 
 end
--- test/algos/quick_union_test.exs
+++ test/algos/quick_union_test.exs
@@ -4,7 +4,6 @@ defmodule QuickUnionTest do
 
   test "QuickUnion.new doesn't error" do
     result = QuickUnion.new 3
-    assert result.data == %{0 => 0, 1 => 1, 2 => 2, 3 => 3}
   end
 
   test "Points are connected to themselves" do
@@ -20,12 +19,17 @@ defmodule QuickUnionTest do
   end
 
   test "union chain works" do
-    data = QuickUnion.new(10)
-      |> QuickUnion.union(0, 1)
+    set = QuickUnion.new(10)
       |> QuickUnion.union(1, 2)
+      |> QuickUnion.union(2, 4)
+      |> QuickUnion.union(5, 0)
+      |> QuickUnion.union(0, 1)
 
-    assert QuickUnion.connected?(data, 0, 2) == true
-    assert QuickUnion.connected?(data, 0, 3) == false
+    assert QuickUnion.connected?(set, 0, 2) == true
+    assert QuickUnion.connected?(set, 0, 3) == false
+    # 5 nodes are connected together with 1 as the root
+    # so the following assertion should be true
+    assert set.data[1] == {1, 5}
   end
 
   test "repition is okay" do
