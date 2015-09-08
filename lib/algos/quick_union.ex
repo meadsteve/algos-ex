ae6c20c8128b35cb329e56e8ed4c6645d03c0760
steve.brazier <steve.brazier@just-eat.com>
2015-09-08 16:35:53 +0100

    add path compression

--- lib/algos/quick_union.ex
+++ lib/algos/quick_union.ex
@@ -10,35 +10,47 @@ defmodule Algos.QuickUnion do
   end
 
   def union(%QuickUnion{data: data}, x, y) when is_integer(x) and is_integer(y) do
-    {root_x, size_x} = get_root(data, x)
-    {root_y, size_y} = get_root(data, y)
-    updated_data = if (size_y > size_x) do
-      data |> update_root(root_y, root_x, (size_x + size_y))
+    {root_x, size_x, nodes_touched_x} = get_root(data, x)
+    {root_y, size_y, nodes_touched_y} = get_root(data, y)
+    if (size_y > size_x) do
+      new_root_data = {root_y, (size_x + size_y)}
+      old_root = root_x
     else
-      data |> update_root(root_x, root_y, (size_x + size_y))
+      new_root_data = {root_x, (size_x + size_y)}
+      old_root = root_y
     end
+    updated_data = data
+      |> update_root(old_root, new_root_data)
+      |> compress(nodes_touched_x, new_root_data)
+      |> compress(nodes_touched_y, new_root_data)
+
     %QuickUnion{data: updated_data}
   end
 
   def connected?(%QuickUnion{data: data}, x, y) when is_integer(x) and is_integer(y) do
-    root_x = get_root(data, x)
-    root_y = get_root(data, y)
+    {root_x, _, _} = get_root(data, x)
+    {root_y, _, _} = get_root(data, y)
     root_x == root_y
   end
 
-  defp get_root(data, i) do
+  defp get_root(data, i, nodes_touched \\ []) do
     {parent, size} = data[i]
     if parent == i do
-      {parent, size}
+      {i, size, nodes_touched}
     else
-      get_root(data, parent)
+      get_root(data, parent, [ i | nodes_touched])
     end
   end
 
-  defp update_root(data, root, old_root, size) do
+  defp update_root(data, old_root, {root, size}) do
     data
       |> Map.put(old_root, {root, 0})
       |> Map.put(root, {root, size})
+  end
+
+  defp compress(data, nodes, {root, size}) do
+    nodes
+      |> Enum.reduce(data, fn(i, map) -> map |> Map.put(i, {root, size}) end)
   end
 
 end
